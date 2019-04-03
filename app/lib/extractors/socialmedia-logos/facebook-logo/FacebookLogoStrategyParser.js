a97eae3263dd48cf3188bf06fd1f1b4b9b286858
Barendrecht <k.barendrecht@scienta.nl>
2019-04-03 14:14:10 +0200

    added social stratgies

---
+++ app/lib/extractors/socialmedia-logos/facebook-logo/FacebookLogoStrategyParser.js
@@ -0,0 +1,21 @@
+class FacebookLogoStrategyParser {
+	document;
+
+	constructor(document) {
+		this.document = document;
+	}
+
+	parse() {
+		let src, node = document.querySelectorAll('a[aria-label="Profile picture"]')[0];
+
+		if (node !== undefined) {
+			src = node.childNodes[0].childNodes[0].getAttribute('src'); // a > div > img
+
+			return {
+				matches: [{src: src, type: 'facebook-logo'}]
+			};
+		}
+
+		return null;
+	}
+}
